e43c2b39d21d2c5713479672ac1a3eb5093133b8
fanjiale <674263280@qq.com>
2016-11-09 16:56:57 +0800

    添加bootstrap管理系统框架

---
+++ webapp/src/main/webapp/manager/app/directive/ng-table.js
@@ -0,0 +1,60 @@
+diamond.directive('ngTable', function () {
+    return {
+        restrict: "EA",
+        scope: {
+            tableconfigs: '='
+        },
+        templateUrl : 'app/directive/ng-table.html',
+        link : {
+            pre : function (scope, element, attrs) {
+
+                element.find('thead').append(scope.$parent.ngTableData.headContent);
+                element.find('tbody').append(scope.$parent.ngTableData.bodyContent);
+
+                var model = attrs['tableconfigs'];
+
+                var config = scope[model];
+
+
+
+                scope.$watch('tableconfigs', function (newVlaue) {
+                    console.log(newVlaue);
+                });
+            },
+            post : function (scope, element, attr) {
+                var model = attr['tableconfigs'];
+
+                var config = scope[model];
+
+
+
+                scope.$watch('tableconfigs', function (newVlaue) {
+                    console.log(newVlaue);
+                });
+            }
+        }
+    }
+}).directive('tableContent', function () {
+    return {
+        restrict: "EA",
+        compile: function(element,qw,asd){
+            var headContent = element.find("thead").html();
+
+            var bodyContent = element.find("tbody").html();
+
+            return {
+                pre: function(scope, iElem, iAttrs){
+                    console.log(name + ': pre link');
+                },
+                post: function(scope, iElem, iAttrs){
+                    scope.ngTableData = {
+                        headContent : headContent,
+                        bodyContent : bodyContent
+                    }
+
+                }
+            }
+
+        }
+    }
+});
--- webapp/src/main/webapp/manager/app/system/user-manager-ctrl.js
+++ webapp/src/main/webapp/manager/app/system/user-manager-ctrl.js
@@ -1,4 +1,20 @@
 diamond.controller('userManagerController', function ($rootScope,$scope,$log,$loggedHttp) {
 
-    $scope.test = "asdasdasd";
+    $scope.list = [
+        {
+            code : '1213123',
+            title : '王金平幕僚：马英九声明字字见血 人活着没意思',
+            user : 'admin',
+            address : '江苏南京',
+            time : '2013-09-09 15:05'
+        }
+    ];
+
+    $scope.tableconfigs = {
+        name : "asdasd",
+        getData : function () {
+            var data = "asdasd";
+            return data;
+        }
+    }
 });
